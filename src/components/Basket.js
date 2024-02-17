7aef3bb67b5320d6709990a565aa41f3abcb00de
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-02-18 07:59:34 +0800

    modify basket layout

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,25 +1,40 @@
+import '../Basket.css'
+
 export default function Basket(){
     return (
         <div className="container-fluid">
-            <nav className="navbar bg-body-tertiary">
+            <nav className="navbar ">
                 <div className="container-fluid">
                     <h3 className="navbar-brand">Shopping Basket</h3>
                     <form className="d-flex">
                     <button className="btn btn-outline-success">Checkout</button>
                     </form>
                 </div>
-                <div className="container-fluid text-left">
-                    <div className="row">
-                        <div className="col">Item</div>
-                        <div className="col">Quantity</div>
-                        <div className="col">Price</div>
-                    </div>
+            </nav>
+            <div className="container-fluid pad">
+                <div className="row">
+                    <div className="col">Item</div>
+                    <div className="col">Quantity</div>
+                    <div className="col">Price</div>
                 </div>
-                <div className="d-flex">
-                    <button>Clear</button>
+            </div>
+
+            <div className="container-fluid pad">
+                <div className="row">
+                    <div className="col">Item</div>
+                    <div className="col">Quantity</div>
+                    <div className="col">Price</div>
+                </div>
+            </div>
+
+            <div className="row d-flex shop-end">
+                <div className="col justify-content-start">
+                    <button className='btn btn-outline-danger'>Clear</button>
+                </div>
+                <div className="col text-end">
                     <h4>Total:</h4>
                 </div>
-            </nav>
+            </div>
         </div>
     )
 }
